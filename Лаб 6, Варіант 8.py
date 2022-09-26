610f18b65da2e8c1757c20aa16ac049bdd6f9d6d
Daryna Isaeva <113431439+isaevadaryna@users.noreply.github.com>
2022-09-26 21:28:34 +0300

    Лаб 6, Варіант 8

---
+++ Лаб 6, Варіант 8.py
@@ -0,0 +1,9 @@
+import math
+a = float(input("Введіть значення a: "))
+b = float(input("Введіть значення b: "))
+h = float(input("Введіть значення h: "))
+for v in range(1, 100):
+    w = math.log(3, math.fabs(v + math.e**v))
+    print(w)
+    if v == b:
+        break
