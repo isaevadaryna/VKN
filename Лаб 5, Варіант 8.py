ab478dd56f5ccda639f65e53bdb18a2587ce3b3b
Daryna Isaeva <113431439+isaevadaryna@users.noreply.github.com>
2022-09-26 09:02:44 +0300

    Update Лаб 5, Варіант 8.py

--- Лаб 5, Варіант 8.py
+++ Лаб 5, Варіант 8.py
@@ -2,12 +2,11 @@ import math
 x = float(input("ВВедіть значня x: "))
 y = 0.0
 if x>=7.2:
-    y = math.log(math.fabs(x+1))
+    y = math.log(4, math.fabs(x+1))
     print(y)
-elif -5.11<x<7.2:
-    y = math.log2(math.fabs(math.cos(x)))**(1./2.)
+elif x<7.2:
+    y = (math.log2(math.fabs(math.cos(x))))**(1./2.)
     print(y)
 elif x<=-5.11:
     y = math.pow(x,2)+4*math.fabs(x-4)+math.e**x
     print(y)
-
