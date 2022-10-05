aa2d91e6ab9d79961b9fde435c52d3adb7db17dd
Daryna Isaeva <113431439+isaevadaryna@users.noreply.github.com>
2022-10-05 11:09:05 +0300

    Update Лаб 6, Варіант 8.py

--- Лаб 6, Варіант 8.py
+++ Лаб 6, Варіант 8.py
@@ -4,6 +4,7 @@ b = float(input("Введіть значення b: "))
 h = float(input("Введіть значення h: "))
 for v in range(1, 100):
     w = math.log(3, math.fabs(v + math.e**v))
+    v = v + h
     print(w)
-    if v == b:
+    if v >= b:
         break
